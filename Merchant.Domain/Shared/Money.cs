5c624a5d507683ea53b8860026cd73041d130096
Oguz Berkay Yerdelen <oyerdel@emea.corpdir.net>
2021-03-26 19:23:13 +0300

    added shared value objects

--- Merchant.Domain/Shared/Money.cs
+++ Merchant.Domain/Shared/Money.cs
@@ -10,22 +10,36 @@ namespace Merchant.Domain.Shared
     public class Money : ValueObject
     {
         public decimal Value { get; }
-        public string CurrencyCode { get; }
-        public string CurrencySymbol { get; set; }
+        public Currency Unit { get; }
+        public Money(decimal value, Currency unit)
+        {
+            Value = value;
+            Unit = unit;
+        }
+
+        
         protected override IEnumerable<object> GetAtomicValues()
         {
-            throw new NotImplementedException();
+            yield return Value;
+            yield return Unit;
         }
     }
-    public class Currency
+    public class Currency : ValueObject
     {
-        public string Unit { get;}
+        public string Unit { get; }
         public Currency(string unit)
         {
+            if (string.IsNullOrEmpty(unit))
+                throw new BusinessException("Unit can not be null or empty.");
             Unit = unit;
         }
         public static Currency TurkishLira => new Currency("TRY");
         public static Currency Euro => new Currency("€");
         public static Currency Dolar => new Currency("$");
+        public static List<string> SupportedCurrencies() => new() { TurkishLira.Unit, Euro.Unit, Dolar.Unit };
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Unit;
+        }
     }
 }
