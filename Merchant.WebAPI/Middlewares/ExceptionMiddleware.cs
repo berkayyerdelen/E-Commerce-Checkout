3ae0e0d1bdee016cab57bf0d882057f84377557c
Oguz Berkay Yerdelen <oyerdel@emea.corpdir.net>
2021-03-27 10:29:08 +0300

    middleware imp

---
+++ Merchant.WebAPI/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using Merchant.Domain.Core.Base;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Merchant.WebAPI.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex) when (ex is BusinessException)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                var error = new ErrorModel
+                {
+                    Code = "404",
+                    Message = ex.Message
+                };
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(error));
+            }
+
+            catch (Exception ex) when (ex is ApplicationException)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.NotAcceptable;
+                var error = new ErrorModel
+                {
+                    Code = "00",
+                    Message = ex.Message
+                };
+
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(error));
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var error = new ErrorModel
+                {
+                    Code = "00",
+                    Message = ex.Message
+                };
+
+                await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(error));
+            }
+        }
+        internal class ErrorModel
+        {
+            public string Code { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
